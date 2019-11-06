b3cd9571f984bb499e42b247cb00185cc017de4a
phamandrew <andrew.w.pham@gmail.com>
2019-11-05 23:43:48 -0500

    working version of submit search

--- src/Homepage.js
+++ src/Homepage.js
@@ -7,15 +7,32 @@ class Homepage extends Component {
     constructor() {
         super();
         this.state = {
-            movies: []
+            movies: [],
+            value: ''
         }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.searchRequest = this.searchRequest.bind(this)
     }
-    componentDidMount() {
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.setState ({
+            value: ''
+        })
+        this.searchRequest();
+    };
+
+    handleChange = (e) => {
+        this.setState({value: e.target.value});
+    }
+    
+    searchRequest() {
         Axios({
-            url: 'https://api.themoviedb.org/3/movie/now_playing',
+            url: 'https://api.themoviedb.org/3/search/movie',
             method: 'GET',
             params: {
-                api_key: moviedb_api_key
+                api_key: moviedb_api_key,
+                query: this.state.value
             }
         })
         .then((res) => {
@@ -24,9 +41,20 @@ class Homepage extends Component {
             });
         });
     }
+
+        
+
     render() {
         return (
             <section className="movie-container">
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        placeholder="Type something to search"
+                    />
+                    <input type="submit" value="Submit" />
+                </form>
                 {this.state.movies.map((movie) => {
                     return (
                         <div className="movie" key={movie.id}>
