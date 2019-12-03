8ee19ad70656e38aacbced9342c5017da88d96be
Andrew Pham <andrew.pham@pts-apham.toronto.exclamation.com>
2019-12-03 15:54:22 -0500

    finally working version of map with results and no errors

--- src/Homepage.js
+++ src/Homepage.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
 import { Link } from 'react-router-dom';
 import { moviedb_api_key } from './App';
 import { search } from './utils';
-import { MovieCreator } from './Movies';
+import MovieCreator, { MovieRender } from './Movies';
 
 class Homepage extends Component {
     constructor() {
@@ -15,7 +15,6 @@ class Homepage extends Component {
         }
         this.handleChange = this.handleChange.bind(this)
         this.searchRequest = this.searchRequest.bind(this)
-        this.renderResults = this.renderResults.bind(this)
     }
    
 
@@ -41,11 +40,10 @@ class Homepage extends Component {
         });
     }
 
-    get renderResults = () => {
+    renderResults = () => {
         let movies = <h1>No Movies : (</h1>;
         if (this.state.movies) {
-        //    movies =  <MovieCreator movieState={this.state.movies} />
-
+            movies = <MovieCreator movieState={this.state.movies} />
         }
 
         return movies;
@@ -64,20 +62,9 @@ class Homepage extends Component {
                     />
 
 
-                
-                {/* {this.state.movies.map((movie) => {
-                    return (
-                        <div className="movie" key={movie.id}>
-                            <Link to={`/movie/${movie.id}`}>
-                                <h3>{movie.original_title}</h3>
-                                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt=""/>
-                            </Link>
-                        </div>
-                    )
-                })}  */}
-                
+                {this.renderResults()};
 
-                {this.renderResults()}
+            </section>
         )
     }
 }
